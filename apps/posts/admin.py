0adc70a7c7c598caa077b45914bb41560afc278e
Okemwag <gabrielokemwa83@gmail.com>
2024-06-13 22:47:45 +0300

    chore: Update admin.py for Post and LikePost models

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,13 +1,67 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import Post, LikePost
 
-# from .models import Post, LikePost
+
+class LikePostInline(admin.TabularInline):
+    model = LikePost
+    extra = 0
+    readonly_fields = ("user", "value")
+    can_delete = False
 
 
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = ['author', 'caption', 'img', 'date_posted', 'date_updated']
-#     search_fields = ['author', 'caption', 'img', 'date_posted', 'date_updated']
-#     list_filter = ['author', 'caption', 'img', 'date_posted', 'date_updated']
-#     list_per_page = 10
-    
-#     class Meta:
-#         model = Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "caption", "date_posted", "date_updated")
+    list_filter = ("date_posted", "author")
+    search_fields = (
+        "author__username",
+        "author__first_name",
+        "author__last_name",
+        "caption",
+    )
+    ordering = ("-date_posted",)
+    readonly_fields = ("date_posted", "date_updated")
+    inlines = [LikePostInline]
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "id",
+                    "author",
+                    "caption",
+                    "img",
+                    "date_posted",
+                    "date_updated",
+                )
+            },
+        ),
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ("author", "id")
+        return self.readonly_fields
+
+
+@admin.register(LikePost)
+class LikePostAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "post", "value")
+    list_filter = ("value", "user")
+    search_fields = (
+        "user__username",
+        "user__first_name",
+        "user__last_name",
+        "post__caption",
+    )
+    ordering = ("-post__date_posted",)
+    readonly_fields = ("user", "post", "value")
+
+    fieldsets = ((None, {"fields": ("id", "user", "post", "value")}),)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ("user", "post")
+        return self.readonly_fields
